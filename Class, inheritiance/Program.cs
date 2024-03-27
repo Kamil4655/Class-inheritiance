87b325e773da64a4ce7ce0cb9bd94b4ccce9b7df
KaMiL374346363 <kamil.qehreman002@gmail.com>
2024-03-28 02:31:32 +0400

    Task homework

---
+++ Class, inheritiance/Program.cs
@@ -0,0 +1,32 @@
+﻿using Class__inheritiance.Models;
+
+namespace Class__inheritiance;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Person studentOne = new Person("Kamil", "Gahramanov", "AB106", 100);
+        studentOne.GetInfo();
+        studentOne.CheckGraduation();
+        studentOne.GetDiplomDegree();
+
+        Console.WriteLine();
+
+        Person studentTwo = new Person("Anar", "Amanli", "AB106", 100);
+        studentTwo.GetInfo();
+        studentTwo.CheckGraduation();
+        studentTwo.GetDiplomDegree();
+
+        Person studentThree = new Person("Rauf", "Dostuyev", "AB106", 75);
+        studentThree.GetInfo();
+        studentThree.CheckGraduation();
+        studentThree.GetDiplomDegree();
+
+        Person studentFour = new Person("Sahib", "Memmedov", "AB106", 55);
+        studentFour.GetInfo();
+        studentFour.CheckGraduation();
+        studentFour.GetDiplomDegree();
+    }
+}
